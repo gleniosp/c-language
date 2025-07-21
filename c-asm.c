e28d0eb6a294e7a278724cddb50c4b874ca05a5c
Glênio Silva Pimentel <11273108+gleniosp@users.noreply.github.com>
2025-07-21 10:00:56 +0000

    31 Demo - if else if and else at assembly level

--- c-asm.c
+++ c-asm.c
@@ -1,8 +1,16 @@
-int glenio() {
-    volatile int i = 10;
+void glenio() {
+    int i = 3;
+    int j = 2;
 
-    i = i + 1;
-    i = i + 10;
+    if (i == 1) {
+        j = j + 1;
+    } else if (i == 2) {
+        j = j + 2;
+    } else if (i == 3) {
+        j = j + 3;
+    } else {
+        j = j + 100;
+    }
 
-    return i;
+    return;
 }

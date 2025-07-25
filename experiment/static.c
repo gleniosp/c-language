520b72efcb1a64a077ae636f77fc5035b4335270
Glênio Silva Pimentel <11273108+gleniosp@users.noreply.github.com>
2025-07-25 09:22:56 +0000

    39 Demo - static keyword - function example

--- experiment/static.c
+++ experiment/static.c
@@ -2,22 +2,9 @@
 
 #include <stdio.h>
 
-extern int a;
-
-void foo();
-
-void bar() {
-    static char b = 3;
-    b = b + 1;
-
-    printf("bar: %d\n", b);
-}
+void bar();
 
 void main() {
-    a = a + 1;
-    printf("main: %d\n", a);
-
-    foo();
     bar(); // b = 4 here
     bar(); // b = 5 here, because b will keep its previous state within the bar function across function calls
     return;
--- experiment/static_aux.c
+++ experiment/static_aux.c
@@ -2,10 +2,21 @@
 
 #include <stdio.h>
 
-int a = 10;
+// if we do "a" and "foo" static, they will be available only in this file and not in other files like static.c, even if we use `extern` to reference them there.
 
-void foo() {
+static int a = 10;
+
+static void foo() {
     a = a + 1;
     printf("foo: %d\n", a);
     return;
+}
+
+// however, the bar function can access "foo" here and as "bar" is not static, we can't use it in static.c
+
+void bar() {
+    static char b = 3;
+    b = b + 1;
+
+    foo();
 }
